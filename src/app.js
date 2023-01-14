2b1c601bdcfd21e1c2626c7d89fcf80081f84233
Viktoriia <117581411+ToriCodesVB@users.noreply.github.com>
2023-01-14 23:53:20 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -70,8 +70,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "ed55b36e362d8733f7d859247cedeaf2";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
- lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
